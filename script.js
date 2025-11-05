aa7b2c1bdbe2cef222837e8e94b8097e67817761
4Mustiere <83943595+4Mustiere@users.noreply.github.com>
2025-11-05 18:05:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 let cookies = 0;
 let farms = 0;
 let factories = 0;
+let bank = 0;
+let temple = 0;
 let boosters = 0;
 
 // Coûts de base
 const baseFarmCost = 15;
 const baseFactoryCost = 100;
 const baseBoosterCost = 100;
+const baseBankCost = 10000;
+const basetempleCost = 100000;
 
 // Audio
 const audio = document.getElementById('click-sound');
@@ -67,6 +71,29 @@ function buyFactory() {
   }
 }
 
+// Acheter une banque
+function buyBank() {
+  const cost = getCost_Prod(baseBankCost, bank);
+  if (cookies >= cost) {
+    cookies -= cost;
+    bank += 1;
+    playSound();
+    updateDisplay();
+  }
+}
+
+
+// Acheter une banque
+function buyBank() {
+  const cost = getCost_Prod(baseBankCost, bank);
+  if (cookies >= cost) {
+    cookies -= cost;
+    bank += 1;
+    playSound();
+    updateDisplay();
+  }
+}
+
 // Acheter un booster
 function buyBooster() {
   const cost = getCost_Booster(baseBoosterCost, boosters);
@@ -80,7 +107,7 @@ function buyBooster() {
 
 // Calcul de la production par seconde
 function calculateCPS() {
-  const baseCPS = farms * 1 + factories * 5;
+  const baseCPS = farms * 1 + factories * 5 + bank * 100;
   const multiplier = Math.pow(2, boosters);
   return baseCPS * multiplier;
 }
@@ -94,12 +121,14 @@ function updateDisplay() {
   document.getElementById('farm-cost').textContent = getCost_Prod(baseFarmCost, farms);
   document.getElementById('factory-cost').textContent = getCost_Prod(baseFactoryCost, factories);
   document.getElementById('booster-cost').textContent = getCost_Booster(baseBoosterCost, boosters);
+  document.getElementById('bank-cost').textContent = getCost_Prod(baseBankCost, bank);
   //Visualisation de si on peut acheter les nouvelles usines ou non
   document.querySelectorAll('.upgrade-item button').forEach((btn, i) => {
     let cost;
     if (i === 0) cost = getCost(baseBoosterCost, boosters);
     else if (i === 1) cost = getCost(baseFarmCost, farms);
     else if (i === 2) cost = getCost(baseFactoryCost, factories);
+    else if (i === 3) cost = getCost(baseBankCost, bank);
     btn.disabled = cookies < cost;
   });
 }
